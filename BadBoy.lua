ec4a24f7d14fc4d82724352a5cf69a517e149dd1
funkydude <funkydude@users.noreply.github.com>
2008-04-17 00:21:30 +0000

    BadBoy: Initial import, a minimal way to block and report spammers

---
+++ BadBoy.lua
@@ -0,0 +1,111 @@
+local ipairs = ipairs
+local fnd = string.find
+
+local triggers = {
+	"100g.ca",
+	"1225game",
+	"29games",
+	"365ige",
+	"5uneed",
+	"agamegold",
+	"agamegoid",
+	"auctionwowhouse",
+	"buywowgame",
+	"buyw0wgame",
+	"buyvvowgame",
+	"championshall",
+	"eusupplier",
+	"eugspa",
+	"fastgg",
+	"gagora",
+	"gamenoble",
+	"gmauthorization",
+	"gmworker",
+	"gmw0rker",
+	"gmworking",
+	"gmw0rking",
+	"gold4guild",
+	"goldwithyou",
+	"goldwow",
+	"helpugame",
+	"heygt",
+	"hugold",
+	"igdollar",
+	"igamebuy",
+	"igm365",
+	"igs36five",
+	"igs365",
+	"gm963",
+	"gs365",
+	"itembay",
+	"itemrate",
+	"iuc365",
+	"kgs",
+	"mmoinn",
+	"mmospa",
+	"ogchannel",
+	"ogmarket",
+	"ogs365",
+	"0gs365",
+	"ogs4u",
+	"okstar2008",
+	"p4hire",
+	"peons4hire",
+	"peons4h1re",
+	"peons4",
+	"4hire",
+	"p3ons",
+	"hir3",
+	"pkpkg",
+	"player123",
+	"scggame",
+	"ssegames",
+	"speedpanda",
+	"terrarpg",
+	"tulongold",
+	"tusongame",
+	"ucgogo",
+	"ucatm",
+	"ukwowgold",
+	"whoyo",
+	"wow4s",
+	"wow7gold",
+	"wowcoming",
+	"wowdupe",
+	"woweurope.cn",
+	"woweuropegold",
+	"wowforever",
+	"wowfreebuy",
+	"wowgoldsky",
+	"wowgoldex",
+	"wowgshop",
+	"wowjx",
+	"wowmine",
+	"wowpanning",
+	"wowpfs",
+	"wowspa",
+	"wowstar2008",
+	"wowsupplier",
+	"wowtoolbox",
+	"zlywy",
+}
+
+local function filter()
+	for _, v in ipairs(triggers) do
+		if fnd(arg1, v) then
+			local dialog = StaticPopup_Show("CONFIRM_REPORT_SPAM_CHAT", arg2)
+			if dialog then
+				dialog.data = arg11
+			end
+			ChatFrame1:AddMessage("|cFF33FF99BadBoy|r: Spam blocked from "..arg2..".")
+			return true
+		end
+	end
+end
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", filter)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_SAY", filter)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", filter)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", filter)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_EMOTE", filter)
+
