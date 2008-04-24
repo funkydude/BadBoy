f27f547945941c5362549810f755d2491da17f92
funkydude <funkydude@users.noreply.github.com>
2008-04-24 23:44:05 +0000

    BadBoy: v1.2 refresh database(reduce false positive chance), increase hit chance

--- BadBoy.lua
+++ BadBoy.lua
@@ -8,92 +8,90 @@ local AUTO_REPORT = true --false otherwise
 
 local triggers = { --list partially taken from SpamSentry, <3
 	--websites
-	"1wowgold",
-	"2wowgold",
-	"29gameswow",
-	"365ige",
-	"5uneed",
-	"51uoo",
-	"agamegold",
-	"bigmouthnest",
-	"championshall%.com",
-	"cheapsgold",
-	"eusupplier",
-	"eugspa",
-	"fastgg",
-	"free%-levels",
-	"gagora%.com",
-	"gamegold123",
-	"gamenoble",
-	"gm365", --ogm
-	"gm963",
-	"gmworker",
-	"gmw0rker",
-	"gmworking",
-	"gmw0rking",
-	"gold230",
-	"gold4guild",
-	"goldwithyou",
-	"goldwow",
-	"happygolds",
-	"helpugame",
-	"heygt",
-	"heypk",
-	"hpygame",
-	"hugold",
-	"igdollar",
-	"igamebuy",
-	"igfad",
-	"igs36", --igs36(five/5)
-	"itembay",
-	"itemrate",
-	"iuc365",
-	"kgsgold",
-	"luckwow%.com",
-	"mmoinn",
-	"mmospa",
-	"ogchannel",
-	"ogmarket",
-	"okstar2008",
-	"ownyo%.com",
-	"pkpkg",
-	"playdone",
-	"player123",
-	"scggame",
-	"scggold",
-	"ssegames",
-	"speedpanda",
-	"susanexpress",
-	"tbgold",
-	"terrarpg",
-	"tgtimes%.com",
-	"torchgame",
-	"tulongold",
-	"tusongame",
-	"ucgogo",
-	"ucatm",
-	"ukwowgold",
-	"whoyo",
-	"wow4s",
-	"wow7gold",
-	"wowcoming",
-	"wow%-europe%.cn",
-	"woweuropegold",
-	"wowfbi",
-	"wowforever",
-	"wowfreebuy",
-	"wowgoldbuy",
-	"wowgoldsky",
-	"wowgoldex",
-	"wowgshop",
-	"wowjx",
-	"wowmine",
-	"wowpanning",
-	"wowseller",
-	"wowspa",
-	"wowsupplier",
-	"wowwar%.net", --www.wowwar.net 24 April 08
-	"yesdaq",
+	"1wowgold%.com", --24 April 08 forward scggold
+	"2wowgold%.com", --24 April 08 forward gmworker
+	"29gameswow", --24 April 08
+	"365ige%.com", --24 April 08 forward gold230
+	"5uneed%.com", --24 April 08
+	"51uoo%.com", --24 April 08
+	"agamegold%.com", --24 April 08
+	"bigmouthnest%.com", --24 April 08 forward yesdaq
+	--"championshall%.com", --24 April 08 Expired
+	"cheapsgold%.com", --24 April 08
+	--"eusupplier%.com", --24 April 08 Expired
+	"eugspa%.com", --24 April 08 forward mmospa
+	"fastgg%.com", --24 April 08
+	"free%-levels%.com", --24 April 08
+	"gagora%.com", --24 April 08
+	"gamegold123%.com", --24 April 08
+	"gamenoble%.com", --24 April 08
+	"gm365%.com", --24 April 08 ogm365/igm365
+	"gm963%.com", --24 April 08
+	"gmworker%.com", --24 April 08
+	"gmworking%.com", --24 April 08
+	"gold230%.com", --24 April 08
+	"gold4guild%.com", --24 April 08
+	--"goldwithyou", --24 April 08 Expired
+	"goldwow%.com", --24 April 08 forward ige
+	"gtgold%.com", --24 April 08 gtgold/heygt
+	"happygolds%.com", --24 April 08
+	"helpugame%.com", --24 April 08
+	"heygt%.com", --24 April 08 heygt/gtgold
+	"heypk%.com", --24 April 08
+	"hpygame%.com", --24 April 08
+	--"hugold", --24 April 08 Expired
+	"igamebuy%.com", --24 April 08
+	"ige%.com", --24 April 08
+	"igfad%.com", --24 April 08
+	"igs365%.com", --24 April 08 forward gmworker
+	"itemrate%.com", --24 April 08
+	"iuc365%.com", --24 April 08
+	"kgsgold%.com", --24 April 08
+	"luckwow%.com", --24 April 08
+	"mmoinn%.com", --24 April 08
+	"mmospa%.com", --24 April 08
+	"ogchannel%.com", --24 April 08
+	--"ogmarket", --24 April 08 Expired
+	--"okstar2008", --24 April 08 Expired
+	"ownyo%.com", --24 April 08
+	"pkpkg%.com", --24 April 08
+	"playdone%.com", --24 April 08
+	--"player123", --24 April 08 Expired
+	"scggame%.com", --24 April 08
+	"scggold%.com", --24 April 08
+	"ssegames%.com", --24 April 08
+	"speedpanda%.com", --24 April 08
+	"susanexpress%.com", --24 April 08
+	"tbgold%.com", --24 April 08
+	"terrarpg%.com", --24 April 08 forward mmoinn
+	"tgtimes%.com", --24 April 08
+	"torchgame%.com", --24 April 08
+	"tulongold%.com", --24 April 08
+	--"tusongame", --24 April 08 Expired
+	"ucgogo%.com", --24 April 08
+	"ucatm%.(%S+)%.tw", --24 April 08 .com/.url
+	"ukwowgold%.com", --24 April 08
+	"whoyo%.com", --24 April 08
+	"wow4s%.com", --24 April 08 forward agamegold
+	"wow7gold%.com", --24 April 08
+	"wowcoming%.com", --24 April 08
+	"wow%-europe%.cn", --24 April 08 forward gmworker
+	--"woweuropegold",  --24 April 08 Expired
+	"wowfbi%.com", --24 April 08 forward gamegold123
+	"wowforever%.com", --24 April 08
+	--"wowfreebuy", --24 April 08 Expired
+	"wowgoldbuy%.net", --24 April 08 forward gm963
+	--"wowgoldsky", --24 April 08 Expired
+	--"wowgoldex%.com", --24 April 08 Expired
+	"wowgshop%.com", --24 April 08
+	"wowjx%.com", --24 April 08 forward wowforever
+	"wowmine%.com", --24 April 08
+	"wowpanning%.com", --24 April 08
+	--"wowseller%.com", --24 April 08 Expired
+	"wowspa%.com", --24 April 08
+	"wowsupplier%.com", --24 April 08
+	"wowwar%.net", --24 April 08 forward wowforever
+	"yesdaq%.com", --24 April 08
 	--phrases
 	"(%d+)poundsper(%d+)gold", -- X pounds per X gold
 	"(%d+)dollarsper(%d+)gold", -- X dollars per X gold
@@ -105,6 +103,7 @@ local function filter()
 	if not CanComplainChat(arg11) then return end
 	local msg = lower(arg1)
 	msg = gsub(msg, " ", "")
+	msg = gsub(msg, ",", ".")
 	for _, v in ipairs(triggers) do
 		if fnd(msg, v) then
 			local time = GetTime()
