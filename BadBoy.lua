3c8fff56e5ae0d36ec9391d2716fd2ff80dfa4cf
funkydude <funkydude@users.noreply.github.com>
2010-06-27 17:59:41 +0000

    anti-goldspam update

--- BadBoy.lua
+++ BadBoy.lua
@@ -118,8 +118,9 @@ local triggers = {
 	--Good Choice ===> MMO4STORE.C0M ==> only (=19.9 per 10k --June 10
 	--Good Choice==> BUYEUGOLD.COM==>Only E17 per 10K --June 10
 	--Good Choice ===> MMO4STORE.CC ==> only E16.36 per 10k --June 10
-	"^goodchoice%W+.*%.c[co0]m?%W+only.*per%d+k$", --Good Choice==>29 [GOLD.COM]==>Only E18 per 10K
-	"^choice%Wm4m%W+.*%.c[co0]m?%W+only.*per%d+k$", --Choice"M4M"=>>BUYEUGOLD.COM=>>Only E13.7 per 10K
+	"^goodchoice%W+.*%.c[co0@]m?%W+only.*per%d+k$", --Good Choice==>29 [GOLD.COM]==>Only E18 per 10K
+	--Choice "M4M"==>WOWGAMELIFE.C@M==>Only E17 per 10K
+	"^choice%Wm4m%W+.*%.c[co0@]m?%W+only.*per%d+k$", --Choice"M4M"=>>BUYEUGOLD.COM=>>Only E13.7 per 10K
 	"sell.*safe.*fast.*site.*gold2wow", --()()Hot selling:safest and fastest trade,reliable site gold2wow()() --June 10
 	"^%W+m+oggg%.[cd][oe]m?%W+$", --April 10
 	"^%W+lastminuteangebotevonmmoggg%W+$", --temp
