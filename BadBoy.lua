2dbcb16df3a9ffaca2851aacba01d99f66368618
funkydude <funkydude@users.noreply.github.com>
2012-03-26 11:52:50 +0000

    anti-goldspam update

--- BadBoy.lua
+++ BadBoy.lua
@@ -108,7 +108,7 @@ local heavyList = {
 --These entries add +2 points, but only 1 entry will count
 local heavyRestrictedList = {
 	"www[%.,●]+",
-	"[%.,●]+c[o0@]?m",
+	"[%.,●]+c[o0@]m",
 	"[%.,●]+net",
 	"dotc[o0@]m",
 }
@@ -338,6 +338,7 @@ local instantReportList = {
 	"[hl]%.?[au]%.?[pc]%.?[pk]%.?y%.?g%.?o%.?l%.?d%.?s.*d%.?e%.?l%.?i%.?v%.?e", --.....H.a.p.p.y.g.o.l.d.s...C.ô.M..........4.99.E. U.R.O.=10.K 10.M.i.n.De.l.i.v.e.r.y..2172
 	"k%.?4%.?g%.?u%.?i%.?l%.?d.*d%.?e%.?l%.?i%.?v%.?e", ----3.W,K.4.G.U.I.L.D,C.@.m 4.5 Êürõ--10k+1O%Disçòünt, Délìvèry 6 M.i.n.s
 	"g[0o]ld.*deliver.*bonus", --3WG0ldsDepot C0M SAVE UP 40% 15Mins DELIVERY 10000=5.99 NEW MEMEBER CAN GET 10% BONUS,NICE CUST0MER ASSISTANT say “NO” to “ ST0LEN G0LD “!!! 
+	"k%.?4%.?p%.?[o0]%.?w%.?e%.?r.*d%.?e%.?l%.?i%.?v%.?e", --3.w,K.4.P.0.W.E.R,c.@.m 4 èü // 1Ok,Délìvèry 6 M.i.n.s
 	--[Gamepowa.net] 3.49e.u.r=5000p.o, le meilleur prix possible ! Recevez votre commande en 5mins. Nous vendons des po depuis plus de 3 ans, plus de 10000 personnes nous ont déjà fait confiance, merci.
 }
 
