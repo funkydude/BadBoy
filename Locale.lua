b93ab312a201eee4a2ca0187183fe651ff211601
funkydude <funkydude@users.noreply.github.com>
2016-10-29 03:35:08 +0100

    prep proper locale

---
+++ Locale.lua
@@ -0,0 +1,23 @@
+
+local _, L = ...
+
+local L = GetLocale()
+if L == "frFR" then
+	
+elseif L == "deDE" then
+	
+elseif L == "zhTW" then
+	
+elseif L == "zhCN" then
+	
+elseif L == "esES" or L == "esMX" then
+	
+elseif L == "ruRU" then
+	
+elseif L == "koKR" then
+	
+elseif L == "ptBR" then
+	
+elseif L == "itIT" then
+	
+end
