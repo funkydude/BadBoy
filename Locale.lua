6737d71a51dbab14d1f33875146dcb9435da7136
funkydude <funkydude@users.noreply.github.com>
2016-11-12 21:21:45 +0000

    update locale

--- Locale.lua
+++ Locale.lua
@@ -1,7 +1,7 @@
 
 local _, L = ...
 
-L.spamBlocked = "Spam blocked, click to report!"
+L.spamBlocked = "BadBoy: Spam blocked"
 L.clickToReport = "|cffeda55fClick|r to report, |cffeda55fAlt-Click|r to dismiss."
 L.spamTooltip = "Show spam in button tooltip"
 L.noAnimate = "Disable button pulse animation"
@@ -9,55 +9,55 @@ L.bigButton = "Make the report button bigger"
 
 local loc = GetLocale()
 if loc == "frFR" then
-	L.spamBlocked = "Spam bloqué, cliquez pour signaler !"
+	L.spamBlocked = "BadBoy: Spam bloqué"
 	L.spamTooltip = "Affichier le spam dans une infobulle"
 	L.noAnimate = "Désactiver l'animation d'impulsion du bouton."
 	--L.clickToReport = "|cffeda55fClick|r to report, |cffeda55fAlt-Click|r to dismiss."
 	--L.bigButton = "Make the report button bigger"
 elseif loc == "deDE" then
-	L.spamBlocked = "Spam geblockt, zum Melden klicken"
+	L.spamBlocked = "BadBoy: Spam geblockt"
 	--L.spamTooltip = "Show spam in button tooltip"
 	--L.noAnimate = "Disable button pulse animation"
 	--L.clickToReport = "|cffeda55fClick|r to report, |cffeda55fAlt-Click|r to dismiss."
 	--L.bigButton = "Make the report button bigger"
 elseif loc == "zhTW" then
-	L.spamBlocked = "垃圾訊息已被阻擋, 點擊以舉報 !"
+	L.spamBlocked = "BadBoy: 垃圾訊息已被阻擋"
 	--L.spamTooltip = "Show spam in button tooltip"
 	--L.noAnimate = "Disable button pulse animation"
 	--L.clickToReport = "|cffeda55fClick|r to report, |cffeda55fAlt-Click|r to dismiss."
 	--L.bigButton = "Make the report button bigger"
 elseif loc == "zhCN" then
-	L.spamBlocked = "垃圾信息已被拦截，点击举报！"
+	L.spamBlocked = "BadBoy: 垃圾信息已被拦截"
 	--L.spamTooltip = "Show spam in button tooltip"
 	--L.noAnimate = "Disable button pulse animation"
 	--L.clickToReport = "|cffeda55fClick|r to report, |cffeda55fAlt-Click|r to dismiss."
 	--L.bigButton = "Make the report button bigger"
 elseif loc == "esES" or loc == "esMX" then
-	L.spamBlocked = "Spam bloqueado, haz clic para reportarlo."
+	L.spamBlocked = "BadBoy: Spam bloqueado"
 	--L.spamTooltip = "Show spam in button tooltip"
 	--L.noAnimate = "Disable button pulse animation"
 	--L.clickToReport = "|cffeda55fClick|r to report, |cffeda55fAlt-Click|r to dismiss."
 	--L.bigButton = "Make the report button bigger"
 elseif loc == "ruRU" then
-	L.spamBlocked = "Спам заблокирован. Нажмите, чтобы сообщить!"
+	L.spamBlocked = "BadBoy: Спам заблокирован"
 	--L.spamTooltip = "Show spam in button tooltip"
 	--L.noAnimate = "Disable button pulse animation"
 	--L.clickToReport = "|cffeda55fClick|r to report, |cffeda55fAlt-Click|r to dismiss."
 	--L.bigButton = "Make the report button bigger"
 elseif loc == "koKR" then
-	--L.spamBlocked = "Spam blocked, click to report!"
+	--L.spamBlocked = "BadBoy: Spam blocked"
 	--L.spamTooltip = "Show spam in button tooltip"
 	--L.noAnimate = "Disable button pulse animation"
 	--L.clickToReport = "|cffeda55fClick|r to report, |cffeda55fAlt-Click|r to dismiss."
 	--L.bigButton = "Make the report button bigger"
 elseif loc == "ptBR" then
-	L.spamBlocked = "Spam bloqueado, clique para denunciar!"
+	L.spamBlocked = "BadBoy: Spam bloqueado"
 	--L.spamTooltip = "Show spam in button tooltip"
 	--L.noAnimate = "Disable button pulse animation"
 	--L.clickToReport = "|cffeda55fClick|r to report, |cffeda55fAlt-Click|r to dismiss."
 	--L.bigButton = "Make the report button bigger"
 elseif loc == "itIT" then
-	L.spamBlocked = "Spam bloccata, clic qui per riportare!"
+	L.spamBlocked = "BadBoy: Spam bloccata"
 	--L.spamTooltip = "Show spam in button tooltip"
 	--L.noAnimate = "Disable button pulse animation"
 	--L.clickToReport = "|cffeda55fClick|r to report, |cffeda55fAlt-Click|r to dismiss."
