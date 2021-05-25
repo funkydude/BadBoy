e24c4f693512fcc3560a67bee7b434841421bb30
funkydude <funkydude@users.noreply.github.com>
2021-05-25 19:46:05 +0100

    Tweaks

--- SimpleOptions.lua
+++ SimpleOptions.lua
@@ -25,7 +25,7 @@ close:SetPoint("TOPRIGHT", badboy, "TOPRIGHT", -5, -5)
 
 local title = badboy:CreateFontString(nil, nil, "GameFontNormalLarge")
 title:SetPoint("TOPLEFT", 16, -16)
-title:SetText("BadBoy @project-version@") -- packager magic, replaced with tag version
+title:SetText("BadBoy")
 
 --[[ Show spam checkbox ]]--
 local btnShowSpam = CreateFrame("CheckButton", nil, badboy, "OptionsBaseCheckButtonTemplate")
